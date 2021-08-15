f7eca49787d7efe04663cced7f316019dca23409
Chantelsky <laver.chantelsky@gmail.com>
2021-08-15 19:54:27 +1000

    adding a mutation to graphql api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 const { ApolloServer } = require('apollo-server');
+// 1 - adding a new integer variable that serves as a very rudimentary way to generate unique IDs for newly created Link elements.
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
 
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`;
-
+// 1 adding a new integer variable that simply serves as a very rudimentary way to generate unique IDs for newly created Link elements.
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    // 2  implementation of the post resolver first creates a new link object, then adds it to the existing links list and finally returns the new link.
+    post: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
   },
 };
 
+const fs = require('fs');
+const path = require('path');
+
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers,
 });
 
